9edcc98b822a37f5eb6ec7613ff19d21fe7c832c
Shubham Rauthan <codexshubham@gmail.com>
2024-11-14 16:07:32 +0530

    Code updated

--- server/Authentication/Authentication.js
+++ server/Authentication/Authentication.js
@@ -7,7 +7,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const authenticate = (req, res, next) => {
     try {
         const token = req.cookies.token;
-
+        console.log(token);
         if (!token) {
             return res.status(401).json({ error: 'No token provided, authorization denied' });
         }
